ee03aa18a55359a3b15d20731c73f0ba8f12b9a4
Rob Hawkes <rob@rawkes.com>
2011-12-08 21:22:07 +0000

    Added platform-specific Node executables

--- chrome/content/node-extension.js
+++ chrome/content/node-extension.js
@@ -2,9 +2,27 @@
     // Import Services module for Observer (startup and shutdown events)
     Components.utils.import("resource://gre/modules/Services.jsm");
 
+    var nodePath;
+
+    switch (Services.appinfo.OS) {
+        case "WINNT": // Windows
+            nodePath = "REPLACE_WITH_ABS_PATH_TO_WINDOWS_NODE_EXECUTABLE";
+            break;
+        case "Darwin": // Mac
+            nodePath = "REPLACE_WITH_ABS_PATH_TO_MAC_NODE_EXECUTABLE";
+            break;
+        case "Linux": // Linux
+            nodePath = "REPLACE_WITH_ABS_PATH_TO_LINUX_NODE_EXECUTABLE";
+            break;
+    };
+
+    if (!nodePath) {
+        return;
+    };
+
     // Create an nsILocalFile for the Node executable
     var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);  
-    file.initWithPath("REPLACE_WITH_ABS_PATH_TO_NODE_EXECUTABLE");
+    file.initWithPath(nodePath);
 
     // Check for Node executable
     if (!file.exists()) {
@@ -32,7 +50,7 @@
                     break;
             };
         }
-    };    
+    };
 
     // Run the Node process and observe for any changes
     var args = ["REPLACE_WITH_ABS_PATH_TO_NODE_SCRIPT"];
